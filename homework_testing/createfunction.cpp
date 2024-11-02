87c4bbba896e84009da10e9fada3c1433791ef6e
Klara Ahinta <176823710+kYra3041@users.noreply.github.com>
2024-11-02 16:09:25 +0000

    added float

--- homework_testing/createfunction.cpp
+++ homework_testing/createfunction.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main () {
+    float calc_sum (unsigned intx[]) {
+        //write codes to compute the sum of the elements of x
+    }
+    
+    float calc_average(unsigned int x[]) {
+        //write codes to compute the average of the elements of x
+
+    }
+
+    float calc_standard_deviation(unsigned int x[]) {
+        //write codes to compute the standard deviation of the elements of x
+    }
 
     return 0;
 }
